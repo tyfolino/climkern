7f49db3b6f52bc6704b40d6faa3f7bff9143aedc
Ty Janoski <janoski@ldeo.columbia.edu>
2024-01-04 17:43:08 -0500

    pre-lint

--- climkern/__init__.py
+++ climkern/__init__.py
@@ -5,3 +5,7 @@ from .frontend import calc_cloud_LW_res, calc_cloud_SW_res
 from .frontend import calc_strato_T, calc_strato_q, calc_RH_feedback
 from .frontend import tutorial_data, spat_avg
 from . import util
+
+__all__ = ['calc_alb_feedbacks','calc_T_feedbacks','calc_q_feedbacks','calc_dCRE_SW',
+           'calc_dCRE_LW','calc_cloud_LW','calc_cloud_SW','calc_cloud_LW_res','calc_cloud_SW_res',
+           'calc_strato_T','calc_strato_q','calc_RH_feedback','tutorial_data','spat_avg','util']
