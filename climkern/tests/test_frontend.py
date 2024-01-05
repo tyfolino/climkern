dffe0f76cd071117db147940290be4c7c4582ccf
Ty Janoski <janoski@ldeo.columbia.edu>
2024-01-05 13:25:05 -0500

    fixed typo

--- climkern/tests/test_frontend.py
+++ climkern/tests/test_frontend.py
@@ -1,22 +1,23 @@
-from climkern.frontend import *
 import xarray as xr
 
-ctrl, pert = tutorial_data('ctrl'),tutorial_data('pert')
+from climkern.frontend import *
+
+ctrl, pert = tutorial_data("ctrl"), tutorial_data("pert")
+
 
 def test_calc_T_feedbacks() -> None:
-    LR,Planck = calc_T_feedbacks(ctrl.T,ctrl.TS,ctrl.PS,
-                                pert.T,pert.TS,pert.PS,pert.TROP_P,
-                                kern="GFDL")
+    LR, Planck = calc_T_feedbacks(
+        ctrl.T, ctrl.TS, ctrl.PS, pert.T, pert.TS, pert.PS, pert.TROP_P, kern="GFDL"
+    )
     dTS_glob_avg = spat_avg(pert.TS - ctrl.TS)
-    LR_val = (spat_avg(LR)/dTS_glob_avg).mean()
-    Planck_val = (spat_avg(Planck)/dTS_glob_avg).mean()
-    xr.testing.assert_allclose(LR_val,xr.DataArray(-0.41),atol=0.01)
-    xr.testing.assert_allclose(Planck_val,xr.DataArray(-3.12),atol=0.01)
+    LR_val = (spat_avg(LR) / dTS_glob_avg).mean()
+    Planck_val = (spat_avg(Planck) / dTS_glob_avg).mean()
+    xr.testing.assert_allclose(LR_val, xr.DataArray(-0.41), atol=0.01)
+    xr.testing.assert_allclose(Planck_val, xr.DataArray(-3.12), atol=0.01)
+
 
 def test_albedo_feedbacks() -> None:
-    alb = calc_alb_feedback(ctrl.FSUS,ctrl.FSDS,
-                           pert.FSUS,pert.FSDS,
-                           kern="GFDL")
+    alb = calc_alb_feedback(ctrl.FSUS, ctrl.FSDS, pert.FSUS, pert.FSDS, kern="GFDL")
     dTS_glob_avg = spat_avg(pert.TS - ctrl.TS)
-    val_to_test = (spat_avg(alb)/dTS_glob_avg).mean()
-    xr.testing.assert_allclose(val_to_test,xr.DataArray(0.38),atom=0.01)
+    val_to_test = (spat_avg(alb) / dTS_glob_avg).mean()
+    xr.testing.assert_allclose(val_to_test, xr.DataArray(0.38), atol=0.01)
