eec6c0e781ea656fdef81f09d159db7ce462bb9b
Ty Janoski <janoski@ldeo.columbia.edu>
2024-01-05 20:09:28 -0500

    removed markdown compat

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ release = '1.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'myst_parser']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
 
 autodoc_default_options = {'members': True}
 autoclass_content = 'class'
